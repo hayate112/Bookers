a24ab195747b9667167f76936052397fbe28c712
hayate112 <k.hayate0112@gmail.com>
2021-05-24 18:19:29 +0000

    [Update]Books Controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,22 +1,49 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
   
   def index
+    @books = Book.all
   end
   
   def show
+    @book = Book.find(params[:id])
   end
   
   def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      flash[:notice] = "必要な項目が未入力です"
+      render :index
+    end
   end
   
   def edit
+    @book = Book.find(params[:id])
   end
   
   def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+    else
+      flash[:notice] = "必要な項目が未入力です"
+      render :edit
+    end
   end
   
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
